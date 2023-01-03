09dcfadf44b28bb43cf478bb933146d0653b9290
Diego Picoli <dgpicoli@gmail.com>
2023-01-03 10:54:40 -0300

    Configura rota post/users

--- src/services/users/CreateUserService.ts
+++ src/services/users/CreateUserService.ts
@@ -1,12 +1,37 @@
+import prismaClient from "../../prisma";
+
 interface UserRequest {
-  name: String;
-  email: String;
-  password: String;
+  name: string;
+  email: string;
+  password: string;
 }
 
 class CreateUserService {
   async execute({name, email, password}: UserRequest) {
-    return { ok: true }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email
+      }
+    })
+
+    if (userAlreadyExists){
+      throw new Error("Usuário já existe")
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      }
+    })
+    return user;
   }
 }
 
